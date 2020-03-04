40811ae9f75d6ce9b507293e4fa288fb345a2d94
razielll <asto1387@gmail.com>
2020-03-04 14:05:51 -0500

    api setup, returns initial data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const BASE_API_URL = 'https://api.500px.com/v1/photos';
+const FEATURE = '?feature=popular';
+const CONSUMER_KEY = '&consumer_key=P7LLhKkPAnPUpbfAXk3Jq2iDjYmCx87zgfEDxQVS';
+// const GET_PHOTOS = `https://api.500px.com/v1/photos?feature=popular&${CONSUMER_KEY}`
 
-export default App;
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      current_page: null,
+      total_pages: null,
+      total_items: null,
+      feature: '',
+      filters: {},
+      photos: [],
+    };
+  };
+
+  componentDidMount = () => {
+    // This isn't working on localhost without a cors disable chrome extension
+    // Seems it's working without a consumer_key also
+    // https://chrome.google.com/webstore/detail/cors-unblock/lfhmikememgdcahcdlaciloancbhjino/related?hl=en
+    fetch(BASE_API_URL + FEATURE + CONSUMER_KEY)
+      .then(response => response.json())
+      .then(data => {
+        console.log('data', data)
+        this.setState(data)
+      });
+  };
+
+  render() {
+    const { photos, current_page, total_pages } = this.state;
+    return (
+      <div className="App">
+        500px challenge!
+    </div>
+    );
+  }
+}
