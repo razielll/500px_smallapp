2fb52c9b09a81a2ba4a40c98f204c0284dcbf3bf
razielll <asto1387@gmail.com>
2020-03-04 18:44:04 -0500

    styles on hover + info

--- src/App.js
+++ src/App.js
@@ -105,6 +105,8 @@ export default class App extends React.Component {
                 <div onClick={() => this.handleModal(photo.id)} className="img-wrapper" key={photo.id + i}>
                   <div className="rating">{photo.rating}⭐</div>
                   <img className="img-resp" src={photo.image_url} alt={photo.name} title={photo.name} />
+                  <div className="user-info"><img src={photo.user.userpic_https_url} height="32" width="32" /> {photo.user.username} </div>
+
                 </div>
               ))}
             </div>
